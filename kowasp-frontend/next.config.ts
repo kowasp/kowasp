8315c93ba3fc8acbe17ab3e5aabc615b1f3eb686
Rony Lee <rongyil33@gmail.com>
2025-03-03 19:45:37 +0800

    chore: deploy on cloudflare pages, add dotenv

--- kowasp-frontend/next.config.ts
+++ kowasp-frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import * as dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
+
+
+if (process.env.NODE_ENV === 'development') {
+  (async () => {
+    await setupDevPlatform();
+  })();
+}
 
 export default nextConfig;
