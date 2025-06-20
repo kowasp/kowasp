336cd093a1c0eeb722295dfac89461076b57eacf
Rony Lee <rongyil33@gmail.com>
2025-06-20 16:47:38 +0800

    chore: remove dist/

--- kowasp-core/dist/src/XSSAnalyzer.d.ts
+++
@@ -1,9 +0,0 @@
-import { XSSFinding } from './types';
-export declare class XSSAnalyzer {
-    constructor();
-    private isHTMLFile;
-    private analyzeHTML;
-    private analyzeJavaScript;
-    analyze(content: string): XSSFinding[];
-    displayResults(findings: XSSFinding[]): void;
-}
--- kowasp-core/dist/test/sample-code.d.ts
+++
@@ -1,6 +0,0 @@
-declare const readFileSync: any;
-declare const XSSScanner: any;
-declare const vulnCode: Array<string>;
-declare const randomLine: string;
-declare const scanner: any;
-declare const findings: any;
